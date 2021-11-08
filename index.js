4eaf026c26322389e17fb48301b8056248632cc2
Danusa Jesus <85651340+DanSiljer@users.noreply.github.com>
2021-11-08 00:12:25 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 const express = require('express');
 const cors = require('cors');
 
-// importar as rotas
+// importar as rotas que eu vou ultilizar
 const presidentesRouter = require('./routes/presidentes.routes');
+
 // inicializacao do express
 const app = express();
+
+
 // habilitar o modo json do express; JSON (Javascript Objective Notation)
 app.use(express.json());
+
 // habilitar o midleware do cors
 app.use(cors());
-//inicializar a rota /presidentes de acordo com as configuracoes do arquivo de rotas
+
+//inicializar a rota /presidentes de acordo com as configurações no meu arquivo de rotas
 app.use('/presidentes', presidentesRouter);
+
 
 const port = 3000;
 app.listen(port, () => {
--- routes/presidentes.routes.js
+++ routes/presidentes.routes.js
@@ -1,36 +1,38 @@
 const express = require('express');
-// inicializar as rotas do express
+
 const router = express.Router();
 
 const presidentes = [
     {
-        id: Math.random(),
+        id: Date.now(),
         nome: 'Floriano Vieira Peixoto',
         partido: 'Militar',
-        imagem:'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Floriano_Peixoto_%281891%29.jpg/214px-Floriano_Peixoto_%281891%29.jpg',
+        img:'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Floriano_Peixoto_%281891%29.jpg/214px-Floriano_Peixoto_%281891%29.jpg',
         ano:'1891 - 1894 ',
+        governo: ' República da Espada',
+        descricao: ' Foi um militar e político brasileiro, primeiro vice-presidente e segundo presidente do Brasil, cujo governo abrange a maior parte do período da história brasileira ',
     },
 ]
 
-// [GET] /presidentes - Retornar uma lista de presidentes
+
 router.get('/', (req, res) => {
     res.send(presidentes);
 })
 
-// [GET] /presidentes/{id} - Retornar um unico presidente por id.
-router.get('/:id', (res, req) => {
-    const idParam = req.params.id;
-    const presidente = presidentes.find(presidente.id == idParam);
 
-    // verifica se o presidente nao foi encontrado
+router.get('/:id', (req, res) => {
+    const idParam = req.params.id;
+    const vaga = presidentes.find(presidente =>  presidente.id == idParam);
+
     if(!presidente) {
-        res.statusCode(404).send({error: 'Presidente não encontrado'});
+        res.status(404).send({error: 'Presidente não encontrada'});
         return;
     }
+
     res.send(presidente);
 })
 
-// [POST] /presidentes/add - Cadastro de uma nova presidente
+
 router.post('/add', (req, res) => {
     const presidente = req.body;
 
@@ -41,10 +43,50 @@ router.post('/add', (req, res) => {
         })
         return;
     }
-    presidente.id = Math.random();
+    presidente.id = Date.now();
     presidentes.push(presidente);
     res.status(201).send({
         menssage: 'Cadastro com sucesso',
         data: presidente
     });
 })
+
+    router.put('/edit/:id', (req, res) => {
+ 
+    const presidenteEdit = req.body;
+    const idParam = req.params.id;
+
+    let index = presidentes.findIndex(presidente => presidente.id == idParam);
+
+    if(index < 0) {
+        res.status(404).send({
+            error: 'O Presidente que você está tentando editar não foi encontrada'
+        })
+        return;
+    }
+
+    presidentes[index] = {
+        ...presidentes[index],
+        ...presidenteEdit
+    }
+
+    res.send({
+        message: `Presidente${Presidentes[index].titulo} atualizado com sucesso`,
+        data: presidentes[index]
+    })
+})
+
+router.delete('/delete/:id', (req, res) => {
+
+    const idParam = req.params.id;
+
+    const index = presidentes.findIndex(presidente => presidente.id == idParam);
+    const nome = presidentes[index];
+   
+    presidentes.splice(index, 1);
+    res.send({
+        message: `Presidente ${nome.nome} excluida com sucesso !`,
+    })
+})
+
+module.exports = router;
